2de0341ddc8a8f30cf122c988082a538205a85f4
Harsh Deshmukh <dnitin111970@gmail.com>
2026-08-02 23:05:39 +0530

    Create Exp_3_Q4.cpp

---
+++ Experiment 3/Exp_3_Q4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float f;
+    int i;
+
+
+    cout << "Enter a float number: ";
+    cin >> f;
+
+    cout << "Enter an integer: ";
+    cin >> i;
+
+
+    int intValue = (int)f;
+    float floatValue = (float)i;
+
+
+    cout << "\nAfter Conversion:\n";
+    cout << "Float to Integer = " << intValue << endl;
+    cout << "Integer to Float = " << floatValue << endl;
+
+    return 0;
+}
