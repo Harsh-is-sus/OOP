25b59a7ab75060e0511b83275e4575067f85f5b3
Harsh Deshmukh <dnitin111970@gmail.com>
2026-08-02 23:10:21 +0530

    Create Exp_3_Q7.cpp

---
+++ Experiment 3/Exp_3_Q7.cpp
@@ -0,0 +1,34 @@
+// Code by Harsh Deshmukh SY CSE-B 15
+// Write a c++ code to create a class which calculates area of a circle.
+
+#include <iostream>
+using namespace std;
+
+class Circle
+{
+private:
+    float radius;
+
+public:
+    void getData()
+    {
+        cout << "Enter the radius: ";
+        cin >> radius;
+    }
+
+    void calculateArea()
+    {
+        float area = 3.14159 * radius * radius;
+        cout << "Area of Circle = " << area << endl;
+    }
+};
+
+int main()
+{
+    Circle c;
+
+    c.getData();
+    c.calculateArea();
+
+    return 0;
+}
